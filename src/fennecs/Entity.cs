2eec479c3b19843b7df2f1cc094717ba0e6a14a0
tiger tiger tiger <tiger@tiger.blue>
2026-07-29 18:27:56 +0200

    chore: clean up linter warnings and redundant qualifiers

--- src/fennecs/Entity.cs
+++ src/fennecs/Entity.cs
@@ -36,7 +36,7 @@ public readonly record struct Entity : IAddRemove<Entity>, IHasTyped, IAddRemove
     /// <para>Applying this to a Query's Stream Type can result in multiple iterations over Entities if they match multiple Component types. This is due to the Wildcard's nature of matching all Components.</para>
     /// </summary>
     /// <inheritdoc cref="Match.Any"/>
-    public static Match Any => new(fennecs.Key.AnyEntity);
+    public static Match Any => new(Key.AnyEntity);
 
     #endregion
 
@@ -50,7 +50,7 @@ public readonly record struct Entity : IAddRemove<Entity>, IHasTyped, IAddRemove
     /// <summary>
     /// The SecondaryKind bits every Entity carries (making its low 48 bits a valid Entity <see cref="Key"/>).
     /// </summary>
-    internal const ulong KindBits = (ulong) SecondaryKind.Entity << fennecs.Key.KindShift;
+    internal const ulong KindBits = (ulong) SecondaryKind.Entity << Key.KindShift;
 
 
     internal Entity(ulong value) => Value = value;
@@ -60,7 +60,7 @@ public readonly record struct Entity : IAddRemove<Entity>, IHasTyped, IAddRemove
     /// Mints an Entity value from its constituents.
     /// </summary>
     internal Entity(byte worldTag, uint index, ushort generation)
-        : this(((ulong) generation << fennecs.Key.GenShift) | KindBits | ((ulong) worldTag << fennecs.Key.WorldShift) | index)
+        : this(((ulong) generation << Key.GenShift) | KindBits | ((ulong) worldTag << Key.WorldShift) | index)
     {
     }
 
@@ -73,19 +73,19 @@ public readonly record struct Entity : IAddRemove<Entity>, IHasTyped, IAddRemove
     /// <summary>
     /// The tag of the World this Entity belongs to.
     /// </summary>
-    internal byte WorldTag => (byte) (Value >> fennecs.Key.WorldShift);
+    internal byte WorldTag => (byte) (Value >> Key.WorldShift);
 
     /// <summary>
     /// <para>Generation discriminator of this Entity.</para>
     /// <para>Whenever an Entity is despawned, the generation of its index is incremented;
     /// stored handles with an outdated generation are no longer <see cref="Alive"/>.</para>
     /// </summary>
-    public ushort Generation => (ushort) (Value >> fennecs.Key.GenShift);
+    public ushort Generation => (ushort) (Value >> Key.GenShift);
 
     /// <summary>
     /// The Key of this Entity, for use as a relation target. (drops the generation)
     /// </summary>
-    internal Key Key => new(Value & fennecs.Key.Mask);
+    internal Key Key => new(Value & Key.Mask);
 
     /// <summary>
     /// The World in which the Entity exists.
